ec403045a81ed6f59b85246bf4320f02f13fa666
kyleleow <kengyeleow@gmail.com>
2021-01-24 18:18:04 +0800

    init: add threejs cdn, scripts and textures

---
+++ app.js
@@ -0,0 +1,31 @@
+//scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight); //set renderer to fill viewport
+document.body.appendChild(renderer.domElement);
+
+//sphere
+const geometry = new THREE.SphereGeometry( 5, 32, 32 );
+const texture = new THREE.TextureLoader().load('textures/moon_1024.jpg');
+const material = new THREE.MeshBasicMaterial({ map: texture });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+scene.add(sphere);
+camera.position.z = 10;
+
+function animate() {
+  requestAnimationFrame(animate); //renderer to draw scene every time screen refreshed
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y -= 0.01;
+  renderer.render(scene, camera);
+};
+
+function updatePOV(newPOV) {
+  camera.fov = newPOV;
+  camera.updateProjectionMatrix();
+}
+
+animate();
